6c99e8b9e54397de833c665393a8ecf2b5aa4ff3
dedahlberg <dwaynedahlberg@gmail.com>
2026-08-19 13:26:25 -0500

    Add latest discounted Red Baron and Bibigo case prices

--- data/products.ts
+++ data/products.ts
@@ -15,7 +15,7 @@ export const products: Product[] = [
   { tcin: '13374407', brand: 'Red Baron', name: 'Four Meat Classic Crust Pizza', category: 'Frozen Pizza', casePrice: 67.20 },
   { tcin: '13334001', brand: 'Red Baron', name: 'Special Deluxe Classic Crust Pizza', category: 'Frozen Pizza' },
   { tcin: '13373589', brand: 'Red Baron', name: 'Pepperoni Thin Crust Pizza', category: 'Frozen Pizza', casePrice: 67.20 },
-  { tcin: '94194523', brand: 'Red Baron', name: 'Pepperoni Stuffed Crust Pizza', category: 'Frozen Pizza' },
+  { tcin: '94194523', brand: 'Red Baron', name: 'Pepperoni Stuffed Crust Pizza', category: 'Frozen Pizza', casePrice: 75.48 },
   { tcin: '87545526', brand: 'Red Baron', name: 'Fully Loaded Pepperoni Pizza', category: 'Frozen Pizza' },
   { tcin: '87545525', brand: 'Red Baron', name: "Fully Loaded Meat Lover's Pizza", category: 'Frozen Pizza', casePrice: 88.06 },
 
@@ -39,7 +39,7 @@ export const products: Product[] = [
   { tcin: '81251225', brand: 'Bibigo', name: 'Mandu Pork & Vegetable Dumplings 24oz', category: 'Dumplings' },
   { tcin: '81251223', brand: 'Bibigo', name: 'Mini Wontons Chicken & Vegetable 24oz', category: 'Wontons' },
   { tcin: '82001058', brand: 'Bibigo', name: 'Mini Wontons Pork & Vegetable 24oz', category: 'Wontons' },
-  { tcin: '87544467', brand: 'Bibigo', name: 'Chicken & Vegetable Steamed Dumplings 6.6oz', category: 'Steamed Dumplings' },
-  { tcin: '87604330', brand: 'Bibigo', name: 'Pork & Vegetable Steamed Dumplings 6.6oz', category: 'Steamed Dumplings' },
-  { tcin: '87544470', brand: 'Bibigo', name: 'Spicy Chicken & Vegetable Crispy Dumpling Bites', category: 'Dumplings' }
+  { tcin: '87544467', brand: 'Bibigo', name: 'Chicken & Vegetable Steamed Dumplings 6.6oz', category: 'Steamed Dumplings', casePrice: 23.31 },
+  { tcin: '87604330', brand: 'Bibigo', name: 'Pork & Vegetable Steamed Dumplings 6.6oz', category: 'Steamed Dumplings', casePrice: 23.31 },
+  { tcin: '87544470', brand: 'Bibigo', name: 'Spicy Chicken & Vegetable Crispy Dumpling Bites', category: 'Dumplings', casePrice: 36.96 }
 ];
